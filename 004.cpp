f4adcb5c1beccfce682c354405077d11bd35d9af
GeGe1005 <sally.westerday@gmail.com>
2025-10-16 22:47:31 -0500

    Update 004.cpp
    Ejercicio 1

--- 004.cpp
+++ 004.cpp
@@ -13,10 +13,9 @@ int main()
 	//Implementamos los operadores ternarios para realizar la ejecucion del resultado.
 	resultado=(tiempo_experiencia >=5) && (nivel_educativo=="Universitaria")?
 	"¡Felicidades! Usted completa los requisitos para la oferta de trabajo. ":
-	(tiempo_experiencia>=3) && (tiempo_experiencia <5) && (nivel_educativo== "Secundaria") ?
+	(tiempo_experiencia>=3) && (tiempo_experiencia <=5) && (nivel_educativo== "Secundaria")?//Necesario cumplir el tiempo de experiencia para que se cumpla el codigo.
 	"¡Felicidades! Usted completa los requisitos para la oferta de trabajo. ":
-	"No aprobado: No cumple con los requisitos.";
+	" No aprobado: No cumple con los requisitos.";
 	cout<<resultado<<endl;
 return 0;
-
 }
